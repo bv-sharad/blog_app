458de6b7964d43638013721f24f4bce0f724e755
vermashid <sharad@blogvault.net>
2022-07-19 06:13:53 +0000

    indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
-  helper_method :current_user, :current_name
+	helper_method :current_user, :current_name
 
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    end
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find(session[:user_id])
+		end
 	end
 	def current_name
 		if session[:user_id]
