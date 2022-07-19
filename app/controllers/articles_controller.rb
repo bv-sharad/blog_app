41754bc63875decc7bd6879cdf3fd4d689b4acc5
vermashid <sharad@blogvault.net>
2022-07-19 07:49:58 +0000

    devise auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,56 +1,49 @@
 class ArticlesController < ApplicationController
-
-	#http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-
-  def index
+	def index
 		@articles = Article.all
-  end
+	end
 
 	def show
-    @article = Article.find(params[:id])
-  end
+		@article = Article.find(params[:id])
+	end
 
 	def new
-    @article = Article.new
-  end
+		@article = Article.new
+	end
 
-  def create
+	def create
 		#article_params[:user] = current_user
-    @article = Article.new(article_params)
-
-    if @article.save
-      redirect_to @article
-    else
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
 			flash[:al] = "Please enter unique title and atleast 10 letters in body"
-      render :new, status: :unprocessable_entity
+			render :new, status: :unprocessable_entity
 			#redirect_to :new, notice: "Please enter unique title and atleast 10 letters in body"
-    end
-  end
-  
+		end
+	end
+
 	def edit
-    @article = Article.find(params[:id])
-  end
+		@article = Article.find(params[:id])
+	end
 
-  def update
-    @article = Article.find(params[:id])
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
 
-    if @article.update(article_params)
-      redirect_to @article
-    else
-			#flash[:nw] = " Error "
-      render :edit, status: :unprocessable_entity
-    end
-  end
-  
 	def destroy
-    @article = Article.find(params[:id])
-    @article.destroy 
+		@article = Article.find(params[:id])
+		@article.destroy 
+		redirect_to root_path, status: :see_other
+	end
 
-    redirect_to root_path, status: :see_other
-  end
-
-  private
-    def article_params
-      params.require(:article).permit(:title, :body, :status, :user)
-    end
+	private
+	def article_params
+		params.require(:article).permit(:title, :body, :status, :user)
+	end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,27 @@
 class CommentsController < ApplicationController
 
-  def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.create(comment_params)
 		puts comment_params
 		if @comment.errors
 			puts @comment.errors.messages
-			 redirect_to article_path(@article)
+			redirect_to article_path(@article)
 		else
 			flash[:alert]
-      redirect_to article_path(@article)
+			redirect_to article_path(@article)
 		end
-  end
-  
-  def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article), status: 303
-  end
+	end
 
-  private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
-    end
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@article), status: 303
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:commenter, :body, :status)
+	end
 end
