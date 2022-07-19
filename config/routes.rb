1a1f8d8c3a98d6e17562922df93b75c0febfd37b
vermashid <sharad@blogvault.net>
2022-07-19 06:29:13 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 	root "articles#index"
-
   resources :articles do
     resources :comments
   end
-
 	get '/signup' => 'users#new'
   post '/users' => 'users#create'
-
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
