b35ecdafe534b6e9484ec0a36ed2195f6028f28c
vermashid <sharad@blogvault.net>
2022-07-21 10:16:02 +0000

    indent

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,19 +1,17 @@
 module Visible
-  extend ActiveSupport::Concern
+        extend ActiveSupport::Concern
 
-  VALID_STATUSES = ['public', 'private', 'archived']
+        VALID_STATUSES = ['public', 'private', 'archived']
 
-  included do
-    validates :status, inclusion: { in: VALID_STATUSES }
-  end
-  
-	class_methods do
-    def public_count
-      where(status: 'public').count
-    end
-  end
-
-  def archived?
-    status == 'archived'
-  end
+        included do
+                validates :status, inclusion: { in: VALID_STATUSES }
+        end
+	      class_methods do
+                def public_count
+                        where(status: 'public').count
+                end
+        end
+        def archived?
+                status == 'archived'
+        end
 end
