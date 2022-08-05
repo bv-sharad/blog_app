fb5b3f984de0d506c5633234566423b02aa231a3
vermashid <sharad@blogvault.net>
2022-08-05 05:23:07 +0000

    indentation

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,54 +1,54 @@
 ActiveAdmin.register Article do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :body, :status, :user, :user_id, :u_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :body, :status, :user, :user_id, :u_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  actions :all, :except => [:destroy, :edit] 
+	# See permitted parameters documentation:
+	# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+	#
+	# Uncomment all parameters which should be permitted for assignment
+	#
+	# permit_params :title, :body, :status, :user, :user_id, :u_id
+	#
+	# or
+	#
+	# permit_params do
+	#   permitted = [:title, :body, :status, :user, :user_id, :u_id]
+	#   permitted << :other if params[:action] == 'create' && current_user.admin?
+	#   permitted
+	# end
+	actions :all, :except => [:destroy, :edit] 
 	permit_params :body, :U, :title, :status
-  
-	filter :id
-  filter :title
-  filter :u_id
 
-  index do
-    selectable_column
-    id_column
-    column :title
+	filter :id
+	filter :title
+	filter :u_id
+
+	index do
+		selectable_column
+		id_column
+		column :title
 		column :created_at
 		column :updated_at
 		column :status
 		column :u
-  end
-  
-	show do
-    attributes_table do
-      row :title
-			row :body
-      row :created_at
-      row :updated_at
-			row :status
-      row :u
-    end
-    active_admin_comments
-  end
+	end
 
-  form do |f|
-    f.inputs do
-      f.input :title
-      f.input :body
-      f.input :status
-    end
-    f.actions
-  end
+	show do
+		attributes_table do
+			row :title
+			row :body
+			row :created_at
+			row :updated_at
+			row :status
+			row :u
+		end
+		active_admin_comments
+	end
+
+	form do |f|
+		f.inputs do
+			f.input :title
+			f.input :body
+			f.input :status
+		end
+		f.actions
+	end
 end
--- app/admin/us.rb
+++ app/admin/us.rb
@@ -1,52 +1,52 @@
 ActiveAdmin.register U do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin, :article_id, :comment_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin, :article_id, :comment_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  actions :all, :except => [:destroy, :edit]
+	# See permitted parameters documentation:
+	# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+	#
+	# Uncomment all parameters which should be permitted for assignment
+	#
+	# permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin, :article_id, :comment_id
+	#
+	# or
+	#
+	# permit_params do
+	#   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin, :article_id, :comment_id]
+	#   permitted << :other if params[:action] == 'create' && current_user.admin?
+	#   permitted
+	# end
+	actions :all, :except => [:destroy, :edit]
 	permit_params :body, :U, :title, :status
-  
+
 	filter :id
-  filter :email
-  filter :articles
+	filter :email
+	filter :articles
 
-  index do
-    selectable_column
-    id_column
-    column :email
-    column :created_at
-    column :updated_at
-    column :articles
-  end
- 
+	index do
+		selectable_column
+		id_column
+		column :email
+		column :created_at
+		column :updated_at
+		column :articles
+	end
+
 	show do
-    attributes_table do
-      row :email
-      row :created_at
-      row :updated_at
-      row :articles
+		attributes_table do
+			row :email
+			row :created_at
+			row :updated_at
+			row :articles
 			row :admin
-    end
-    active_admin_comments
-  end
+		end
+		active_admin_comments
+	end
 
-  form do |f|
-    f.inputs do
-      f.input :title
-      f.input :body
-      f.input :status
-    end
-    f.actions
-  end
+	form do |f|
+		f.inputs do
+			f.input :title
+			f.input :body
+			f.input :status
+		end
+		f.actions
+	end
 end
