f1c068e0ec10e3b54fe706ba41496b5433a1fa59
vermashid <sharad@blogvault.net>
2022-08-05 05:14:38 +0000

    review changes

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -14,6 +14,41 @@ ActiveAdmin.register Article do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  actions :all, :except => [:destroy, :edit] 
+	permit_params :body, :U, :title, :status
   
-	permit_params :body
+	filter :id
+  filter :title
+  filter :u_id
+
+  index do
+    selectable_column
+    id_column
+    column :title
+		column :created_at
+		column :updated_at
+		column :status
+		column :u
+  end
+  
+	show do
+    attributes_table do
+      row :title
+			row :body
+      row :created_at
+      row :updated_at
+			row :status
+      row :u
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :body
+      f.input :status
+    end
+    f.actions
+  end
 end
--- app/admin/us.rb
+++ app/admin/us.rb
@@ -14,5 +14,39 @@ ActiveAdmin.register U do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  permit_params :email, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin 
+  actions :all, :except => [:destroy, :edit]
+	permit_params :body, :U, :title, :status
+  
+	filter :id
+  filter :email
+  filter :articles
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :created_at
+    column :updated_at
+    column :articles
+  end
+ 
+	show do
+    attributes_table do
+      row :email
+      row :created_at
+      row :updated_at
+      row :articles
+			row :admin
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :body
+      f.input :status
+    end
+    f.actions
+  end
 end
